1aa7fd5738bf5560f38f0c41734f50e59c46f090
MalarvizhiK <k.malarvizhi@in.ibm.com>
2020-07-02 14:07:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,23 +5,6 @@ data "ibm_is_region" "region" {
   name = var.region
 }
 
-
-terraform {
-  backend "s3" {
-    bucket                      = var.cos_bucket
-    key                         = var.cos_state_file
-    region                      = var.cos_region
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_get_ec2_platforms      = true
-    skip_requesting_account_id  = true
-    skip_metadata_api_check     = true
-    endpoint                    = var.cos_endpoint
-    access_key                  = var.cos_access_key
-    secret_key                  = var.cos_secret_key
-  }
-}
-
 data "ibm_resource_group" "rg" {
   name = var.resource_group
 }
