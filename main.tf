102590a245f6a27e3adf7ff7949a9648612a579d
MalarvizhiK <k.malarvizhi@in.ibm.com>
2020-07-03 11:34:04 +0530

    To test CD pipeline

--- main.tf
+++ main.tf
@@ -8,10 +8,9 @@ data "ibm_is_region" "region" {
 data "ibm_resource_group" "rg" {
   name = var.resource_group
 }
-/*
+
 resource "ibm_is_vpc" "test-vpc" {
   name = var.vpc_name
   resource_group = data.ibm_resource_group.rg.id
 }
-*/
 
